39336d45cc7e9e2b473846ef18d38010aaca31c8
Shawn Patel <shawnpatel@berkeley.edu>
2024-11-10 21:34:24 -0800

    almond train systemd service

---
+++ almond_train.sh
@@ -0,0 +1,11 @@
+cd /home/ubuntu/almond-intelligence
+conda activate lerobot
+
+DATA_DIR=data python lerobot/scripts/train.py \
+    dataset_repo_id=shawnptl8/koch_test_training \
+    policy=act_koch_real \
+    env=koch_real \
+    hydra.run.dir=outputs/train/act_koch_test_training \
+    hydra.job.name=act_koch_test_training \
+    device=cpu \
+    wandb.enable=true
